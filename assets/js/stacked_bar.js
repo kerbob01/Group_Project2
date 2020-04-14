a0d43eed104bc1212a5f5ef6102952e3fac613c1
Alan Dalton <58486363+kerbob01@users.noreply.github.com>
2020-04-14 11:20:29 -0600

    updated bar chart

--- assets/js/stacked_bar.js
+++ assets/js/stacked_bar.js
@@ -13,7 +13,7 @@ var svg = d3.select("#my_dataviz")
     "translate(" + margin.left + "," + margin.top + ")");
 
 // Parse the Data
-d3.csv("../data/counts_by_season_state_piviot.csv", function (data) {
+d3.csv("counts_by_season_state_piviot.csv", function (data) {
 
   var keys = ["Fall", "Spring", "Summer", "Unknown", "Winter"]
 
@@ -45,15 +45,56 @@ d3.csv("../data/counts_by_season_state_piviot.csv", function (data) {
   var color = d3.scaleOrdinal()
     .domain(subgroups)
     .range(["#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e"])
-  //["#1f77b4","#ff7f0e","#2ca02c","#d62728","#9467bd","#8c564b","#e377c2","#7f7f7f","#bcbd22","#17becf"]
-  //["#1b9e77","#d95f02","#7570b3","#e7298a","#66a61e","#e6ab02","#a6761d","#666666"]
-
-
+  
   //stack the data? --> stack per subgroup
   var stackedData = d3.stack()
     .keys(subgroups)
     (data)
   console.log(stackedData)
+
+  
+  // ----------------
+  // Highlight a specific subgroup when hovered
+  // ----------------
+  var Tooltip = d3.select("#my_dataviz")
+  .append("div")
+  .style("opacity", 0)
+  .attr("class", "tooltip")
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "2px")
+  .style("border-radius", "5px")
+  .style("padding", "5px")
+
+  
+  // What happens when user hover a bar
+  var mouseover = function (d) {
+    // what subgroup are we hovering?
+    var subgroupName = d3.select(this.parentNode).datum().key; // This was the tricky part
+    console.log(subgroupName)
+    var subgroupValue = d.data[subgroupName];
+    // Reduce opacity of all rect to 0.2
+    d3.selectAll(".myRect").style("opacity", 0.2)
+    // Highlight all rects of this subgroup with opacity 0.8. It is possible to select them since they have a specific class = their name.
+    d3.selectAll("." + subgroupName)
+      .style("opacity", 1)
+  }
+//------------------------------------------------------------------------------------------------
+  //adds popup of counts
+  // var mousemove = function (d) {
+  //   Tooltip
+  //     .html("Number of Sightings: " + stackedData)
+  //     .style("left", (d3.mouse(this)[0] + 40) + "px")
+  //     .style("top", (d3.mouse(this)[1]) + "px")
+  // }
+
+  // When user do not hover anymore
+  var mouseleave = function (d) {
+    // Back to normal opacity: 0.8
+    d3.selectAll(".myRect")
+      .style("opacity", 0.8)
+  }
+
 
   // Show the bars
   svg.append("g")
@@ -62,19 +103,21 @@ d3.csv("../data/counts_by_season_state_piviot.csv", function (data) {
     .data(stackedData)
     .enter().append("g")
     .attr("fill", function (d) { return color(d.key); })
+    .attr("class", function (d) { return "myRect " + d.key }) // Add a class to each subgroup: their name
     .selectAll("rect")
     // enter a second time = loop subgroup per subgroup to add all rectangles
     .data(function (d) { return d; })
     .enter().append("rect")
     .attr("x", function (d) { return x(d.data.State); })
-    .attr("width", x.bandwidth())
-    .attr("height", function(d) { return y(d[0]) - y(d[1]); })
-  //set up for animation
-    //.attr("height", function (d) { return height - y(0); }) // always equal to 0
     .attr("y", function (d) { return y(d[1]); })
+    .attr("height", function (d) { return y(d[0]) - y(d[1]); })
+    .attr("width", x.bandwidth())
+    .attr("stroke", "grey")
+    .on("mouseover", mouseover)
+    //.on("mousemove", mousemove)
+    .on("mouseleave", mouseleave)
 
-
-//animation to load bar chart
+  //animation to load bar chart
   // svg.selectAll("rect")
   //   .transition()
   //   .duration(2000)
@@ -82,7 +125,7 @@ d3.csv("../data/counts_by_season_state_piviot.csv", function (data) {
   //   .attr("height", function (d) { return height - y(stackedData); })
   //   .delay(function (d, i) { console.log(i); return (i * 100) })
 
-//Ledgend layout
+  //Ledgend layout
   // Add one dot in the legend for each name.
   var size = 15
   svg.selectAll("mydots")
@@ -106,5 +149,7 @@ d3.csv("../data/counts_by_season_state_piviot.csv", function (data) {
     .text(function (d) { return d })
     .attr("text-anchor", "left")
     .style("alignment-baseline", "middle")
+
+
 
 })
